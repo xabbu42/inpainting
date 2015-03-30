9320bd0a84536ae650fbc04b8ba66206b28249b8
Nathan Gass <gass@search.ch>
2015-03-30 10:54:43 +0200

    Use corrected forw_variation in import_common.m

--- inpainting_NATHANGASS.m
+++ inpainting_NATHANGASS.m
@@ -3,12 +3,10 @@ function [u] = inpainting_NATHANGASS(g,omega,lambda)
 %        omega: mask
 % lambda: parameter % output: u: inpainted image
 import_common();
-adj  = @(u) conv2(u, [0 1 0; 1 0 1; 0 1 0], 'same');
-tau  = @(u) sqrt(forx(u, 0) .^ 2 + fory(u, 0) .^ 2);
-cost = @(u) (lambda/2) * sum2(omega .* (u - g) .^ 2) + sum2(tau(u));
-grad = @(u) lambda * (omega .* (u - g)) + (1 ./ tau(u)) .* (4 * u - adj(u));
+cost = @(u) (lambda/2) * sum2(omega .* (u - g) .^ 2) + forw_total_variation(u);
+grad = @(u) lambda * (omega .* (u - g)) + forw_total_variation_grad(u);
 
-u = gradient_descent(g, cost, grad);
+u = gradient_descent(g, cost, grad, 'iterations', 100);
 
 return;
 
