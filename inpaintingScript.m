323af6964d3928a982e53f806083e04606ff7fd7
Nathan Gass <gass@search.ch>
2015-03-30 11:33:17 +0200

    Add scale for easy generation of smaller problems. Saner default for lambda.

--- inpaintingScript.m
+++ inpaintingScript.m
@@ -3,18 +3,19 @@ clear all;
 close all;
 clc;
 
-im = im2single(rgb2gray(imread('grumpycat.jpeg')));
+scale = 0.5;
+im = imresize(im2single(rgb2gray(imread('grumpycat.jpeg'))), scale);
 
 % mask
 omega = ones(size(im));
-omega(195:209,31:134) = 0;
-omega(31:65,166:194) = 0;
+omega(unique(round((195:209) * scale)), unique(round((31:134) * scale))) = 0;
+omega(unique(round((31:65) * scale)), unique(round((166:194) * scale))) = 0;
 
 % create input image
 g = im.*omega;
 
 
-lambda = 1000;
+lambda = 0.1;
 
 uG = inpainting_NATHANGASS(g,omega,lambda);
 
