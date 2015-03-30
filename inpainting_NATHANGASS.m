626a421688c5e60579b8f10132bc42169577a0b9
Nathan Gass <gass@search.ch>
2015-03-30 11:40:21 +0200

    Use default iterations of gradient_descent.m. Pass on extra arguments to gradient_descent.

--- inpainting_NATHANGASS.m
+++ inpainting_NATHANGASS.m
@@ -1,4 +1,4 @@
-function [u] = inpainting_NATHANGASS(g,omega,lambda)
+function [u] = inpainting_NATHANGASS(g,omega,lambda,varargin)
 % input: g: single gray scaled image
 %        omega: mask
 % lambda: parameter % output: u: inpainted image
@@ -6,7 +6,7 @@ import_common();
 cost = @(u) (lambda/2) * sum2(omega .* (u - g) .^ 2) + forw_total_variation(u);
 grad = @(u) lambda * (omega .* (u - g)) + forw_total_variation_grad(u);
 
-[u, meta] = gradient_descent(g, cost, grad, 'iterations', 1000);
+[u, meta] = gradient_descent(g, cost, grad, varargin{:});
 
 meta
 
