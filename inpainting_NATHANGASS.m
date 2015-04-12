a98eea2e72d40b2b8a2f6ed8c3193bb34eeef4ef
Nathan Gass <gass@search.ch>
2015-04-12 18:52:30 +0200

    Add some whitespace.

--- inpainting_NATHANGASS.m
+++ inpainting_NATHANGASS.m
@@ -2,7 +2,9 @@ function [u] = inpainting_NATHANGASS(g,omega,lambda,varargin)
 % input: g: single gray scaled image
 %        omega: mask
 % lambda: parameter % output: u: inpainted image
+
 import_common();
+
 cost = @(u) (lambda/2) * sum2(omega .* (u - g) .^ 2) + forw_total_variation(u);
 grad = @(u) lambda * (omega .* (u - g)) + forw_total_variation_grad(u);
 
