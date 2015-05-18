49c502df4911007225477731a40448ce7d2a83a0
Nathan Gass <gass@search.ch>
2015-05-18 13:27:07 +0200

    Use same lambda for all 3 variants.

--- inpaintingScript.m
+++ inpaintingScript.m
@@ -19,7 +19,7 @@ lambda = 200;
 
 uG = inpainting_NATHANGASS(g,omega,lambda, 'plot', 1, 'iterations', 1000, 'sigma', 0.5, 'tau', 0.5, 'theta', 0.5);
 %uG = inpainting_NATHANGASS(g,omega,lambda, 'alpha', 1/3, 'beta', 0.5, 'plot', 1, 'iterations', 5000);
-%uG = inpainting_NATHANGASS(g, omega, 50, 'constantstep', 3e-3, 'plot', 1, 'iterations', 2000);
+%uG = inpainting_NATHANGASS(g, omega,lambda, 'constantstep', 3e-3, 'plot', 1, 'iterations', 2000);
 
 di = abs(g - uG);
 di = di / max(di(:));
