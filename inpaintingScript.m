7568d04877aef26d3cba6ae3d42884f3d818ca89
Nathan Gass <gass@search.ch>
2015-05-23 00:10:48 +0200

    The parameters finaly used.

--- inpaintingScript.m
+++ inpaintingScript.m
@@ -14,10 +14,9 @@ omega(unique(round((31:65) * scale)), unique(round((166:194) * scale))) = 0;
 % create input image
 g = im.*omega;
 
-
 lambda = 200;
 
-uG = inpainting_NATHANGASS(g,omega,lambda, 'plot', 1, 'iterations', 1000, 'sigma', 0.5, 'tau', 0.5, 'theta', 0.5);
+uG = inpainting_NATHANGASS(g,omega,lambda, 'plot', 1, 'iterations', 600, 'sigma', 0.5, 'tau', 0.5, 'theta', 0.2);
 %uG = inpainting_NATHANGASS(g,omega,lambda, 'alpha', 1/3, 'beta', 0.5, 'plot', 1, 'iterations', 5000);
 %uG = inpainting_NATHANGASS(g, omega,lambda, 'constantstep', 3e-3, 'plot', 1, 'iterations', 2000);
 
