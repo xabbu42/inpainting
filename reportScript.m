9d5966926d3003ced326a83ee93f460774854823
Nathan Gass <gass@search.ch>
2015-05-22 23:52:39 +0200

    adapt to generate report images for gradient descent and primal dual

--- reportScript.m
+++ reportScript.m
@@ -8,9 +8,6 @@ import_common;
 im = im2single(rgb2gray(imread('grumpycat.jpeg')));
 imwrite(im, 'original.png');
 
-% gradient descent args
-args = {'alpha', 1e-2, 'beta', 0.5, 'iterations', 2500};
-
 % mask
 omega = ones(size(im));
 omega(195:209,31:134) = 0;
@@ -20,15 +17,20 @@ omega(31:65,166:194) = 0;
 g = im.*omega;
 imwrite(g, 'input.png');
 
+args = {'method', 'gradientdescent', 'alpha', 1/3, 'beta', 0.5, 'iterations', 5000};
+%args = {'method', 'primaldual', 'sigma', 0.5, 'tau', 0.5, 'theta', 0.2, 'iterations', 600};
+
+name = args{2};
+
 % different stages
 early = inpainting_NATHANGASS(g, omega,   100, args{:}, 'iterations', 10);
 mid   = inpainting_NATHANGASS(g, omega,   100, args{:}, 'iterations', 1000);
 late  = inpainting_NATHANGASS(g, omega,   100, args{:}, 'iterations', 2500);
 final = inpainting_NATHANGASS(g, omega,   100, args{:}, 'iterations', 5000);
-imwrite(early, 'early.png');
-imwrite(mid,   'mid.png');
-imwrite(late,  'late.png');
-imwrite(final, 'final.png');
+imwrite(early, strcat(name, '-early.png'));
+imwrite(mid,   strcat(name, '-mid.png'));
+imwrite(late,  strcat(name, '-late.png'));
+imwrite(final, strcat(name, '-final.png'));
 
 % different lambdas
 vlow = inpainting_NATHANGASS(g, omega,  10, args{:});
@@ -36,10 +38,10 @@ low  = inpainting_NATHANGASS(g, omega,  50, args{:});
 good = inpainting_NATHANGASS(g, omega, 100, args{:});
 high = inpainting_NATHANGASS(g, omega, 500, args{:});
 
-imwrite(vlow, 'vlow_lambda.png');
-imwrite(low,  'low_lambda.png');
-imwrite(good, 'good_lambda.png');
-imwrite(high, 'high_lambda.png');
+imwrite(vlow, strcat(name, '-vlow_lambda.png'));
+imwrite(low,  strcat(name, '-low_lambda.png'));
+imwrite(good, strcat(name, '-good_lambda.png'));
+imwrite(high, strcat(name, '-high_lambda.png'));
 
 % plot lambda vs ssd to original
 lambda = linspace(1, 500, 50);
@@ -47,7 +49,7 @@ ssd = zeros(10, 1);
 besti = 1;
 i = 1;
 for i = 1:length(lambda)
-    lambda(i)
+	lambda(i)
 	res = inpainting_NATHANGASS(g, omega, lambda(i), args{:});
 	ssd(i) = qnorm2(res - im);
 	if (ssd(i) < ssd(besti))
@@ -56,18 +58,16 @@ for i = 1:length(lambda)
 	end
 end
 
-save('plotdata.mat', 'lambda', 'ssd', 'besti');
+save(strcat(name, '-plotdata.mat'), 'lambda', 'ssd', 'besti');
 
 lambda(besti)
 ssd(besti)
 
-imwrite(best, 'best.png');
+imwrite(best, strcat(name, '-best.png'));
 
 plot(lambda, ssd);
 xlabel('lambda');
 ylabel('SSD');
 set(gca,'FontSize', 10);
 set(findall(gcf,'type','text'), 'FontSize', 12, 'fontWeight','bold');
-print('plot', '-dpng');
-
-
+print(strcat(name, '-plot'), '-dpng');
